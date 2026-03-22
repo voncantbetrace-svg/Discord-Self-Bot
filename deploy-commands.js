44ce0e6ea8303d7fa639827fe819f27f7b130445
voncantbetrace-svg <voncantbetrace@gmail.com>
2026-03-22 13:33:51 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,6 @@
 const { REST, Routes } = require('discord.js');
 require('dotenv').config();
 
-// Your exact command object
 const commands = [
   {
     name: 'flood',
@@ -31,7 +30,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
   try {
     console.log('Registering guild slash commands...');
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), // Guild-specific
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
       { body: commands }
     );
     console.log('Successfully registered guild commands.');
