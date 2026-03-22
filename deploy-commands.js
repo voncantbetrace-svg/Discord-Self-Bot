deed05f9016cda66bbfc17877b8df45172d22571
voncantbetrace-svg <voncantbetrace@gmail.com>
2026-03-22 13:05:52 -0400

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,35 @@
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+require('dotenv').config();
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('flood')
+    .setDescription('Send a burst of messages safely')
+    .addStringOption(option =>
+      option.setName('message')
+        .setDescription('The message you want to send')
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option.setName('count')
+        .setDescription('Number of times to send (1-10)')
+        .setMinValue(1)
+        .setMaxValue(10)
+    )
+    .toJSON()
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Registering slash commands...');
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+    console.log('Successfully registered commands.');
+  } catch (err) {
+    console.error(err);
+  }
+})();
