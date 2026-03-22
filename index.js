b90efbacb3ef41b3876f8b3737301d8fdd2e1818
voncantbetrace-svg <voncantbetrace@gmail.com>
2026-03-22 00:46:20 -0400

    Update bot online message with a checkmark

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
 // Create the client
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds
-  ]
+  intents: [GatewayIntentBits.Guilds] // minimal required intent
 });
 
 // Bot ready event
 client.once('ready', () => {
-  console.log(`Bot is online as ${client.user.tag}!`);
+  console.log(`✅ Bot is online as ${client.user.tag}!`);
 });
 
 // Login
