8f9e24fd900c2039cd062c60e8b747502f8e5320
voncantbetrace-svg <voncantbetrace@gmail.com>
2026-03-22 09:11:37 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  token: 'MTQ4NTE1OTE0NTQ0NjU3MjA5Mw.GfImLM.aZzRJ5PBIl6NB6VanbkLv6f8m9yBy6r6uwrZgI',
+  token: 'MTQ4NTE1OTE0NTQ0NjU3MjA5Mw.G8u9e9.rRWLAIFQwAxXpv9BRaXDEgx0KJ1QXWEZbTMhOU',
   clientId: '1484719905243795506',
   guildId: '1484257224053231618', // optional
   logChannelId: '1484257224053231618' // channel for logs
