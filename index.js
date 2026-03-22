c304dd07cb624ae254c9959aa55fe9394330de38
voncantbetrace-svg <voncantbetrace@gmail.com>
2026-03-21 21:36:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-// index.js
 const { Client, GatewayIntentBits } = require('discord.js');
 
-// Create a client instance
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
-// When the bot is ready
 client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Bot is online as ${client.user.tag}!`);
 });
 
-// Example message command
 client.on('messageCreate', message => {
-  if (message.author.bot) return; // ignore bots
-  if (message.content === '!ping') message.channel.send('Pong!');
+  if (message.author.bot) return;
+  if (message.content === '!ping') message.reply('Pong!');
 });
 
-// Login using the TOKEN environment variable (set on Railway)
 client.login(process.env.TOKEN);
