49a5f39dee9dbd4aa50c3ef25b59cf93e45199e4
voncantbetrace-svg <voncantbetrace@gmail.com>
2026-03-22 09:02:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  token: 'MTQ4NDcxOTkwNTI0Mzc5NTUwNg.G05nTk.RDkG5EYhZiUVgxaRcl73EqURs2wNFfq4xk548c',
+  token: 'MTQ4NTE1OTE0NTQ0NjU3MjA5Mw.GfImLM.aZzRJ5PBIl6NB6VanbkLv6f8m9yBy6r6uwrZgI',
   clientId: '1484719905243795506',
   guildId: '1484257224053231618', // optional
   logChannelId: '1484257224053231618' // channel for logs
