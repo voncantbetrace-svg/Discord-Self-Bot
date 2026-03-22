6caa75f4a1f7346ef87ad93e6c5aa04a79ef4853
voncantbetrace-svg <voncantbetrace@gmail.com>
2026-03-22 09:15:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ client.once('ready', () => {
 });
 
 // 👇 THIS goes at the very bottom
-client.login("MTQ4NTE1OTE0NTQ0NjU3MjA5Mw.GfImLM.aZzRJ5PBIl6NB6VanbkLv6f8m9yBy6r6uwrZgI");
+client.login("MTQ4NTE1OTE0NTQ0NjU3MjA5Mw.G8u9e9.rRWLAIFQwAxXpv9BRaXDEgx0KJ1QXWEZbTMhOU");
