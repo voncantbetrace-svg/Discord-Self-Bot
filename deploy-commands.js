0f26cddd2b4047b0f28968d20d78d9a485658317
voncantbetrace-svg <voncantbetrace@gmail.com>
2026-03-22 17:28:42 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,13 +1,13 @@
 // deploy-commands.js
-require('dotenv').config(); // Load environment variables
-const { REST } = require('@discordjs/rest'); // Import REST once
-const { Routes, SlashCommandBuilder } = require('discord.js'); // Import Routes & SlashCommandBuilder
+require('dotenv').config();
+const { REST } = require('@discordjs/rest');
+const { Routes, SlashCommandBuilder } = require('discord.js');
 
 // Define your commands
 const commands = [
   new SlashCommandBuilder()
     .setName('flood')
-    .setDescription('Send messages with Di5 emblem') // Changed here
+    .setDescription('Send messages with Di5 emblem')
     .addStringOption(option =>
       option.setName('message')
             .setDescription('Message to send')
@@ -17,7 +17,7 @@ const commands = [
             .setDescription('Number of times to send (1-16)'))
 ].map(cmd => cmd.toJSON());
 
-// Create REST client (only once)
+// Create REST client
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
@@ -32,5 +32,3 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
     console.error('Error registering commands:', err);
   }
 })();
-
-
