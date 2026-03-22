5f1c8223cc571bb86f1405b93075f486343f1b6a
voncantbetrace-svg <voncantbetrace@gmail.com>
2026-03-22 09:26:49 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
-
-module.exports = {
-  token: 'MTQ4NTE1OTE0NTQ0NjU3MjA5Mw.G8u9e9.rRWLAIFQwAxXpv9BRaXDEgx0KJ1QXWEZbTMhOU',
-  clientId: '1484719905243795506',
-  guildId: '1484257224053231618', // optional
-  logChannelId: '1484257224053231618' // channel for logs
-};
+{
+  "token": "MTQ4NTI2Nzg0MjU5MjAxNDUzMQ.GxputI.KtsQzRYgmEU9LONXm16wz7nXmLtZr3QSb6AAS4"
+}
