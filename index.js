078fbfd84d515afd68ec9024806b1d7abe536403
voncantbetrace-svg <voncantbetrace@gmail.com>
2026-03-22 12:40:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
 client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Bot is online as ${client.user.tag}!`);
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN).catch(err => {
+  console.error("Failed to login. Check your TOKEN:", err);
+  process.exit(1); // exits safely if token is invalid
+});
