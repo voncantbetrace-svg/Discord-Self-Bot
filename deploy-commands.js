8ad234da25d29d1265a19f4fb192a47720020672
voncantbetrace-svg <voncantbetrace@gmail.com>
2026-03-22 14:02:54 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -8,14 +8,14 @@ const commands = [
     options: [
       {
         name: 'message',
-        type: 3, // STRING
-        description: 'The message you want to send',
+        type: 3,
+        description: 'The message to send',
         required: true
       },
       {
         name: 'count',
-        type: 4, // INTEGER
-        description: 'How many times to send (1-10)',
+        type: 4,
+        description: 'Amount (1-10)',
         required: false,
         min_value: 1,
         max_value: 10
@@ -28,12 +28,14 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log('Registering guild slash commands...');
+    console.log('Registering GLOBAL slash commands...');
+    
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      Routes.applicationCommands(process.env.CLIENT_ID), // GLOBAL
       { body: commands }
     );
-    console.log('Successfully registered guild commands.');
+
+    console.log('Global commands registered (may take up to 1 hour to show).');
   } catch (err) {
     console.error(err);
   }
